8a774833adc0b8bcf7113413a2fddf225a2e5808
Chiara <chiaralagana.219@gmail.com>
2020-09-30 18:54:10 +0200

    commit 2

--- main.js
+++ main.js
@@ -17,18 +17,21 @@ $(document).ready(function () {
 
 });
 
-
+var posts;
 
 function addPost() {
     var title = $("#postTitle").val();
     var content = $("#postContent").val();
 
     var post = new Post(title, content, true);
+    posts.push(post);
 
     var article = '<div class="col-md-4 card"><div class="card"><header class="card-header">'+ post.title + '</header><img src="..." class="card-img-top" alt="..."><article class="card-text"><h5 class="card-title">'+ post.title +
     '</h5><p class="card-text">'+ post.content + '</p></article><footer></footer></div>'
 
     $(".articles").append(article);
+
+    return posts;
 }
 
 function removeAllPosts() {
@@ -41,11 +44,13 @@ function generatePosts(){
     var post3 = new Post("Sais", "blablabla", true);
     var post4 = new Post("Pas", "blablabla", true);
 
-    var posts = [post1, post2, post3, post4];
+    
 
     var article1 = '<div class="col-md-4 card"><div class="card"><header class="card-header">' + post1.title + '</header><img src="..." class="card-img-top" alt="..."><article class="card-text"><h5 class="card-title">' + post1.title + '</h5><p class="card-text">' + post1.content + '</p></article><footer></footer></div>'
     var article2 = '<div class="col-md-4 card"><div class="card"><header class="card-header">' + post2.title + '</header><img src="..." class="card-img-top" alt="..."><article class="card-text"><h5 class="card-title">' + post2.title + '</h5><p class="card-text">' + post2.content + '</p></article><footer></footer></div>'
     var article3 = '<div class="col-md-4 card"><div class="card"><header class="card-header">' + post3.title + '</header><img src="..." class="card-img-top" alt="..."><article class="card-text"><h5 class="card-title">' + post3.title + '</h5><p class="card-text">' + post3.content + '</p></article><footer></footer></div>'
     var article4 = '<div class="col-md-4 card"><div class="card"><header class="card-header">' + post4.title + '</header><img src="..." class="card-img-top" alt="..."><article class="card-text"><h5 class="card-title">' + post4.title + '</h5><p class="card-text">' + post4.content + '</p></article><footer></footer></div>'
     $(".articles").append(article1, article2, article3, article4);
+
+    return posts = [post1, post2, post3, post4];
 }
